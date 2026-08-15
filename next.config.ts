d3d904316d3e1bd6faa94bcc2c282625f34d7fb9
SPPG Admin <admin@sppg.local>
2026-08-16 05:35:44 +0700

    fix: remove output standalone for vercel

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   // PWA will be added once next-pwa is confirmed installed
   // Allow mobile testing via IP
   allowedDevOrigins: ['192.168.1.3', '192.168.1.1', 'localhost', '029aafaa4af5df32-103-130-18-153.serveousercontent.com'],
-  output: "standalone",
+
   experimental: {
   },
 };
