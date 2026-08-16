317bf20a56717a8645cf4c13bc5bda474dbc16ae
SPPG Admin <admin@sppg.local>
2026-08-16 18:18:51 +0700

    chore: remove scripts directory

--- scripts/check-tables.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '../lib/db';
-import { sql } from 'drizzle-orm';
-
-async function main() {
-  const res = await db.execute(sql`SELECT table_name FROM information_schema.tables WHERE table_schema='public'`);
-  console.log(res);
-}
-
-main().catch(console.error);
